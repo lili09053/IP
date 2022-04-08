cc05882a87c7d5170ea46c1eb2af44f35eb1dc35
lili09053 <91773645+lili09053@users.noreply.github.com>
2022-04-08 21:30:26 +0500

    Add files via upload

---
+++ dijkstra.py
@@ -0,0 +1,53 @@
+distance_vertex = []
+visited_vertex = []
+max_len = 10000
+matrix = \
+    [
+        [0, 7, 9, 0, 0, 14],
+        [0, 0, 10, 15, 0, 0],
+        [0, 0, 0, 11, 0, 2],
+        [0, 0, 0, 0, 6, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 9, 0]
+    ]
+
+
+def qwerty():
+    start_vertex = int(input("Введите вершину: "))
+    dijkstra(start_vertex - 1)
+
+
+def dijkstra(start_vertex):
+    v = start_vertex + 1
+    for i in range(len(matrix)):
+        distance_vertex.append(max_len)
+        visited_vertex.append(False)
+
+    distance_vertex[start_vertex] = 0
+
+    for count in range(len(matrix)):
+        index = 0
+        min = max_len
+
+        for i in range(len(matrix)):
+            if visited_vertex[i] != True and distance_vertex[i] <= min:
+                min = distance_vertex[i]
+                index = i
+
+        j = index
+        visited_vertex[j] = True
+
+        for i in range(len(matrix)):
+            if visited_vertex[i] != True and matrix[j][i] and \
+                distance_vertex[j] != max_len and \
+                distance_vertex[j] + matrix[j][i] < distance_vertex[i]:
+                distance_vertex[i] = distance_vertex[j] + matrix[j][i]
+
+    for i in range(len(matrix)):
+        print(v, " -> ", i + 1, " = ", end="")
+        if distance_vertex[i] != max_len:
+            print(distance_vertex[i])
+        else:
+            print("нет пути домой")
+
+qwerty()
